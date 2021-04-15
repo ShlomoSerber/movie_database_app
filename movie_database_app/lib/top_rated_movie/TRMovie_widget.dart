3219508d5af6a84cb20048245c4b0aff3087be32
ShlomoSerber <ss.nefg@gmail.com>
2021-04-15 14:33:09 -0300

    commit

---
+++ movie_database_app/lib/top_rated_movie/TRMovie_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TRMovieWidget extends StatefulWidget {
+  @override
+  _TRMovieWidgetState createState() => _TRMovieWidgetState();
+}
+
+class _TRMovieWidgetState extends State<TRMovieWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
