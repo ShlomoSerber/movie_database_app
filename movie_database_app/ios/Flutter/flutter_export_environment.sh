6af1cd4f9d538cc05ae84a121b53a41595c39c49
ShlomoSerber <ss.nefg@gmail.com>
2021-04-15 14:05:41 -0300

    commit

--- movie_database_app/ios/Flutter/flutter_export_environment.sh
+++ movie_database_app/ios/Flutter/flutter_export_environment.sh
@@ -2,7 +2,6 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/shlomo/snap/flutter/common/flutter"
 export "FLUTTER_APPLICATION_PATH=/home/shlomo/Desktop/Shlomo/Programing/Dart/Curso Dart/Nivelacion/movie_database_app/movie_database_app"
-export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
--- movie_database_app/lib/main/main.dart
+++ movie_database_app/lib/main/main.dart
@@ -1,1 +1,167 @@
-void main() {}
+// To parse this JSON data, do
+//
+//     final welcome = welcomeFromJson(jsonString);
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+
+Welcome welcomeFromJson(String str) => Welcome.fromJson(json.decode(str));
+
+String welcomeToJson(Welcome data) => json.encode(data.toJson());
+
+class Welcome {
+  Welcome({
+    this.page,
+    this.results,
+    this.totalPages,
+    this.totalResults,
+  });
+
+  int page;
+  List<Result> results;
+  int totalPages;
+  int totalResults;
+
+  factory Welcome.fromJson(Map<dynamic, dynamic> json) => Welcome(
+        page: json["page"],
+        results:
+            List<Result>.from(json["results"].map((x) => Result.fromJson(x))),
+        totalPages: json["total_pages"],
+        totalResults: json["total_results"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "page": page,
+        "results": List<dynamic>.from(results.map((x) => x.toJson())),
+        "total_pages": totalPages,
+        "total_results": totalResults,
+      };
+}
+
+class Result {
+  Result({
+    this.adult,
+    this.backdropPath,
+    this.genreIds,
+    this.id,
+    this.originalLanguage,
+    this.originalTitle,
+    this.overview,
+    this.popularity,
+    this.posterPath,
+    this.releaseDate,
+    this.title,
+    this.video,
+    this.voteAverage,
+    this.voteCount,
+  });
+
+  bool adult;
+  String backdropPath;
+  List<int> genreIds;
+  int id;
+  String originalLanguage;
+  String originalTitle;
+  String overview;
+  double popularity;
+  String posterPath;
+  DateTime releaseDate;
+  String title;
+  bool video;
+  double voteAverage;
+  int voteCount;
+
+  factory Result.fromJson(Map<String, dynamic> json) => Result(
+        adult: json["adult"],
+        backdropPath: json["backdrop_path"],
+        genreIds: List<int>.from(json["genre_ids"].map((x) => x)),
+        id: json["id"],
+        originalLanguage: json["original_language"],
+        originalTitle: json["original_title"],
+        overview: json["overview"],
+        popularity: json["popularity"].toDouble(),
+        posterPath: json["poster_path"],
+        releaseDate: DateTime.parse(json["release_date"]),
+        title: json["title"],
+        video: json["video"],
+        voteAverage: json["vote_average"].toDouble(),
+        voteCount: json["vote_count"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "adult": adult,
+        "backdrop_path": backdropPath,
+        "genre_ids": List<dynamic>.from(genreIds.map((x) => x)),
+        "id": id,
+        "original_language": originalLanguage,
+        "original_title": originalTitle,
+        "overview": overview,
+        "popularity": popularity,
+        "poster_path": posterPath,
+        "release_date":
+            "${releaseDate.year.toString().padLeft(4, '0')}-${releaseDate.month.toString().padLeft(2, '0')}-${releaseDate.day.toString().padLeft(2, '0')}",
+        "title": title,
+        "video": video,
+        "vote_average": voteAverage,
+        "vote_count": voteCount,
+      };
+}
+
+const String restHttp =
+    'https://api.themoviedb.org/3/movie/top_rated?api_key=0e685fd77fb3d76874a3ac26e0db8a4b&language=en-US';
+
+Future<List> bodyList(int pageNumber) async {
+  List<Result> videosList = [];
+
+  var result =
+      await http.get(Uri.parse(restHttp + '&page=' + pageNumber.toString()));
+  if (result.statusCode == 200) {
+    final Map parsed = json.decode(result.body);
+    for (int x = 0; x < parsed.length; x++) {
+      videosList.add(Welcome.fromJson(parsed).results[x]);
+    }
+  }
+
+  return videosList;
+}
+
+main(List<String> args) {
+  runApp(MaterialApp(home: Test()));
+}
+
+class Test extends StatefulWidget {
+  @override
+  _TestState createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  Welcome results;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder(
+          future: http.get(Uri.parse(restHttp)),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+
+            if (!snapshot.hasData) {
+              return Center(child: Text('No hay información'));
+            }
+
+            final Map parsed = json.decode(snapshot.data.body);
+            results = Welcome.fromJson(parsed);
+
+            return ListView.builder(
+              itemBuilder: (context, index) {
+                return Card(child: Text(results.results[index].title));
+              },
+              itemCount: results.results.length,
+            );
+          }),
+    );
+  }
+}
