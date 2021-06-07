41674a63e9253d788442a518b633c49fa0988a30
ShlomoSerber <ss.nefg@gmail.com>
2021-06-07 16:31:07 -0300

    commit

---
+++ movie_database_app/lib/searched_movie/SearchButton_widget.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:movie_database_app/global_theme/global_theme.dart';
+import 'package:movie_database_app/searched_movie/MainSMovie_widget.dart';
+
+class SearchButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    TextEditingController controller = TextEditingController();
+    String query;
+
+    return IconButton(
+      icon: Icon(Icons.search),
+      onPressed: () {
+        return AlertDialog(
+          title: Text(
+            'Search',
+            style: TextStyle(color: appBarColor),
+          ),
+          content: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                style: TextStyle(fontSize: width * 0.03),
+                decoration: InputDecoration(labelText: 'Search'),
+                controller: controller,
+                textAlign: TextAlign.start,
+                keyboardType: TextInputType.text,
+                onChanged: (value) {
+                  query = value;
+                },
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Search'),
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MainSMovie(query),
+                  ),
+                );
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
